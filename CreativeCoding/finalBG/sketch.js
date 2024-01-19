407c7780ecaeea55ff8145dfe68f1330cccf2ca3
cchoy21 <144940371+cchoy21@users.noreply.github.com>
2024-01-19 12:11:04 -0500

    update

--- CreativeCoding/finalBG/sketch.js
+++ CreativeCoding/finalBG/sketch.js
@@ -1,7 +1,35 @@
+    //adding a GIF to my grid
+
+    let gif;
+
+    function preload (){
+    gif = loadtiImage ('assets/Giftransparent.gif');
+    }
+
 function setup() {
-  createCanvas(400, 400);
+  //runs one time
+  createCanvas(windowWidth, windowHeight);
+  
+  
 }
 
 function draw() {
-  background(220);
+  //runs in a loop
+  
+  background('red');
+
+    vars num = 50;
+    vars sideLen = windowWidth/num;
+
+    for (var y = 0; y < windowHeight; y = y +sideLen) {
+      //loop to create a grid in the y direction
+
+      for(var x = 0; x < windowWidth; x = x +sideLen) {
+image (gif, x, y, sideLen, sideLen);        
+      }
+    }
+  }
+
+    function windowResized (){
+      resizeCanvas (windowWidth, windowHeight);
 }
